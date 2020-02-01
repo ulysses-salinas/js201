d012b4f1f9c1d15eea2eef04e6b5eb4bbcfab738
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-01-31 20:43:03 -0600

    yay - I finished exercise 205-tip-calculator.js

--- exercises/205-tip-calculator.js
+++ exercises/205-tip-calculator.js
@@ -10,8 +10,18 @@
 // tipAmount(100, 'good') --> 20
 // tipAmount(40, 'fair') --> 6
 
-
-
+function tipAmount (bill, service){
+    if (service === 'good'){
+        return bill * .2;
+    }
+    if (service === 'fair'){
+        return bill * .15;
+    }
+    if (service === 'poor'){
+        return bill * .1;
+    }
+}
+tipAmount(100, 'poor')
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "totalAmount" that takes the same arguments as "tipAmount"
 // except it returns the total as the tip amount plus the bill amount.
@@ -20,9 +30,19 @@
 // Examples:
 // totalAmount(100, 'good') --> 120
 // totalAmount(40, 'fair') --> 46
+function totalAmount (bill, service) {
+    if (service === 'good'){
+        return bill + tipAmount(bill, service);
+    }
+    if (service === 'fair'){
+        return bill + tipAmount(bill, service);
+    }
+    if (service === 'poor'){
+        return bill + tipAmount(bill, service);
+    }
+}
 
-
-
+totalAmount(100, 'good')
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "splitAmount" that takes a bill amount, the level of service,
 // and the number of people to split the bill between. It should return the final
@@ -31,3 +51,15 @@
 // Examples:
 // splitAmount(100, 'good', 5) --> 24
 // splitAmount(40, 'fair', 2) --> 23
+function splitAmount (bill, service, people){
+    if (service === 'good'){
+        return (bill + tipAmount(bill, service)) / people
+    }
+    if (service === 'fair'){
+        return (bill + tipAmount(bill, service)) / people
+    }
+    if (service === 'poor'){
+        return (bill + tipAmount(bill, service)) / people
+    }
+}
+console.log(splitAmount(30, 'poor', 3))
