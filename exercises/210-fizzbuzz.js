b7d6123d063d36d54f9743da78adeb9e1c8f3551
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-02-04 18:05:04 -0600

    yay - I finished exercise 210-fizzbuzz.js

--- exercises/210-fizzbuzz.js
+++ exercises/210-fizzbuzz.js
@@ -9,3 +9,35 @@
 // Examples:
 // fizzbuzz(3) --> '..fizz'
 // fizzbuzz(15) --> '..fizz.buzzfizz..fizzbuzz.fizz..fizzbuzz'
+
+//function fizzbuzz (num) {
+//for (var i = 1; i <= num; i++)
+//if (i % 15 === 0) console.log('fizzbuzz'); 
+//else if (i % 3 === 0) console.log('fizz');
+//else if (i % 5 === 0) console.log('buzz');
+//else console.log('.');
+
+//}
+
+
+function fizzbuzz (number) {
+    let str = '';
+    let arr = [];
+    for (let i = 1; i <= number; i++){
+        arr.push(i)
+    }
+    for (let i in arr){
+        if (arr[i] % 3 !== 0 && arr[i] % 5 !== 0){
+            str += '.'
+        }
+        else if (arr[i] % 3 === 0 && arr[i] % 5 !== 0){
+            str += 'fizz'
+        }
+        else if (arr[i] % 3 !== 0 && arr[i] % 5 === 0){
+            str += 'buzz'
+        }
+        else { str += 'fizzbuzz'
+     }
+  }
+ return str
+}
