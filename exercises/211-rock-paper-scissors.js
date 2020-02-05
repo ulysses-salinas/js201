c3899fd01884ad40cafaece48853e4dfbb6a7bd9
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-02-04 22:18:48 -0600

    yay - I finished exercise 211-rock-paper-scissors.js

--- exercises/211-rock-paper-scissors.js
+++ exercises/211-rock-paper-scissors.js
@@ -6,3 +6,22 @@
 // rockPaperScissors('rock', 'scissors') --> 'player 1'
 // rockPaperScissors('rock', 'paper') --> 'player 2'
 // rockPaperScissors('paper', 'paper') --> 'draw'
+
+function rockPaperScissors (throw1, throw2){
+    let arr = ['rock','paper','scissors']
+    if (throw1 === arr[0] & throw2 === arr[1]){
+      return 'player 2'
+    }
+    else if (throw1 === arr[1] && throw2 === arr[2]){
+      return 'player 2'
+    }
+    else if (throw1 === arr[2] && throw2 === arr[0]){
+      return 'player 2'
+    }
+    else if (throw1 === throw2){
+      return 'draw'
+    }
+    else {return 'player 1'
+    }
+    }
+    rockPaperScissors('paper','rock')
