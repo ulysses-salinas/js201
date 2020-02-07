4bbb131bc67cccf39507f0b43e082e7ce6d83de8
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-02-06 23:11:48 -0600

    yay - I finished exercise 212-number-joiners.js

--- exercises/212-number-joiners.js
+++ exercises/212-number-joiners.js
@@ -5,12 +5,37 @@
 // numberJoinerWhile(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerWhile(12, 14) --> '12_13_14'
 
-
+function numberJoinerWhile (num1, num2){
+    str = ''
+    let i = num1;
+    while (i <= num2){
+     if (i >= num1 && i < num2){
+          str += i++ + '_' 
+        }
+        else if (i = num2){
+          str += i++
+        }
+    }
+    return str
+    
+    }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFor" which does the same thing as "numberJoinerWhile",
 // except using a "for" loop internally.
-
+function numberJoinerFor (num1, num2){
+    str = '';
+     for (let i = num1; i <= num2; i++){
+      if (i >= num1 && i < num2){
+        str += i + '_' 
+      }
+      else if (i = num2){
+        str += i
+      }
+    }
+    return str
+  }
+  numberJoinerFor(1,10)
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -21,3 +46,22 @@
 // numberJoinerFancy(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerFancy(1, 5, '~') --> 1~2~3~4~5
 // numberJoinerFancy(3, 6, '***BANANAS***') --> 1***BANANAS***2***BANANAS***3
+
+
+function numberJoinerFancy (num1, num2, seperator){
+    let str = ''
+    for (let i = num1; i <= num2; i++){
+        if (i >= num1 && i < num2 && seperator === undefined){
+            str= str + i + '_'
+        }
+        else if (i >= num1 && i < num2 && seperator !== -1){
+        str= str + i + seperator
+    }
+        else {
+           str = str + i
+        }
+    }
+    return str
+    
+    
+    }
