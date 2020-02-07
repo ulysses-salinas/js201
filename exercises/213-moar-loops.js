6737f0fdf854d68a540d041e2321fa461195b48d
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-02-06 19:13:57 -0600

    yay - I finished exercise 213-moar-loops.js

--- exercises/213-moar-loops.js
+++ exercises/213-moar-loops.js
@@ -1,12 +1,11 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Complete the removeZAnimals function as described below:
 
-function removeZAnimals () {
   // 1) declare an array with some strings
-  const animals = ["alligator", "zebra", "crocodile", "giraffe"]
+ 
 
   // create an empty array (we will fill this with strings from the previous array)
-  let animalsWithoutZ = []
+
 
   // 2) loop through "animals"
 
@@ -16,15 +15,39 @@ function removeZAnimals () {
 
   // 4) return "animalsWithoutZ"
 
-}
+  function removeZAnimals (){
+    const animals = ['alligator','zebra','crocodile','giraffe'];
+    let animalsWithoutZ = [];
+    
+    for (let i = 0; i <= 3; i++){
+      if (animals[i] === animals[i] && animals[i].indexOf('z') === -1 ){
+        animalsWithoutZ.push(animals[i])
+      }
+    
+    }
+    return animalsWithoutZ
+    }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "removeAnyWordWithZ" that takes 1 argument: an array of strings
 // It should return a new array that has all of the items in the passed-in array minus
 // any words that contain the letter 'z' or 'Z' (case-insensitive)
 
+function removeAnyWordWithZ (arr) {
 
-
+  let newArr = [];
+  
+  
+  for (let i in arr){
+    if (arr[i].search(/Z/i) === -1){
+         newArr.push(arr[i])
+    }
+    
+  }
+  return newArr
+  }
+  removeAnyWordWithZ(['Zelda','Xzylophone', 'onomatopoeia', 'Zeezee'])
+  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "removeWordsWithChar" that takes 2 arguments:
 // 1) an array of strings
@@ -35,3 +58,12 @@ function removeZAnimals () {
 // Examples:
 // removeWordsWithChar(['aaa', 'bbb', 'ccc'], 'b') --> ['aaa', 'ccc']
 // removeWordsWithChar(['pizza', 'beer', 'cheese'], 'E') --> ['pizza']
+function removeWordsWithChar (arr, char) {
+  let newArr = [];
+  for (let i in arr){
+    if (!arr[i].includes(char.toLowerCase())){
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
